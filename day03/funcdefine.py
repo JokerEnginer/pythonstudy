251935a9672717a693504655f4f2df87824f059e
JokerEnginer <42333661+JokerEnginer@users.noreply.github.com>
2021-12-17 18:35:36 +0800

    func change

--- day03/funcdefine.py
+++ day03/funcdefine.py
@@ -50,13 +50,51 @@ avg = avg_num(11, 22, 34)
 print(avg)
 
 """
+"""
+# 全局变量跟局部变量
+a = 0   #定义全局变量
+def fun1():
+    global a #对全局变量进行修改,如果是字典和列表做全局变量,函数内部使用可以不用global修饰
+    a = 32
+def fun2():
+    print("a=%d"%a)
+fun1()
+fun2()
+# 局部变量与全局变量名字相同
+如果定义了局部变量,优先使用局部变量,如果没有定义局部变量,会使用全局变量
 
+a = 10
+def fun1():
+    a = 20
+    print(a)
+def fun2():
+    print(a)
+fun1()
+fun2()
+"""
+"""
+缺省参数
+        1.定义函数给形参传递初始值
+        2.函数调用需要给指定参数传值,不传使用形参初始值.如果调用函数传值
+        变量名需要跟形参一致
 
+def fun1(a, b, c=33):
+    print(a)
+    print(b)
+    print(c)
+fun1(11, 22)
+fun1(11, 22, c=44)
+# fun1(a=1, b=2,A=44) 报错了,命名参数与形参不一致
+"""
+# 不定长参数
 
-
-
-
-
+def num1(a, b, *args):
+    sumnum = 0
+    for i in args:
+        sumnum += i
+    result = sumnum+a+b
+    print(result)
+num1(1,2,3,4,5,6,7)
 
 
 
